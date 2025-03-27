de496df1ef5b80e65c607670a9a6005ba70ef165
HwangHyeonseok <hhs0995@naver.com>
2025-03-27 10:23:33 +0900

    lecture: 6-2_회원 웹 기능 - 등록(@GetMapping, @PostMapping, html <form> 태그)

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,22 +1,44 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
 
-    private MemberService memberService;
-
-    // setter 주입
+    // 생성자 주입(추천)
+    private final MemberService memberService;
     @Autowired
-    public void setMemberService(MemberService memberService) {
+    public MemberController(MemberService memberService) {
         this.memberService = memberService;
     }
 
-    //    @Autowired
-//    public MemberController(MemberService memberService) {
+    // 객체 주입 (비추천)
+//    @Autowired private MemberService memberService;
+
+    // setter 주입 (비추천)
+//    private MemberService memberService;
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
 //        this.memberService = memberService;
 //    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
