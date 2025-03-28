7efd12e631cf795b55a43611e4a0d311a5831f89
HwangHyeonseok <hhs0995@naver.com>
2025-03-28 15:03:53 +0900

    lecture: 8-2_AOP 적용

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,25 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component // bean 등록
+@Aspect // AOP 사용을 위해서!
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))") // hello.hellospring 패키지 하위에는 다 적용
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            Object result = joinPoint.proceed();
+            return result;
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,19 +21,12 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
-        long start = System.currentTimeMillis();
 
         // 같은 이름이 있는 중복 회원 X
-        try {
-            Optional<Member> result = memberRepository.findByName(member.getName());
-            validateDuplicateMember(result);
-            memberRepository.save(member);
-            return member.getId();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish-start;
-            System.out.println("join = " + timeMs + "ms");
-        }
+        Optional<Member> result = memberRepository.findByName(member.getName());
+        validateDuplicateMember(result);
+        memberRepository.save(member);
+        return member.getId();
     }
 
     // 중복 회원 검증
@@ -45,14 +38,8 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        long start = System.currentTimeMillis();
-        try {
-            return memberRepository.findAll();
-        } finally {
-            long finish = System.currentTimeMillis();
-            long timeMs = finish - start;
-            System.out.println("findMembers = " + timeMs + "ms");
-        }
+
+        return memberRepository.findAll();
     }
 
     // ID에 맞는 1명 조회
