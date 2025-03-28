6e1cc2ae8334f8f76a6bfed281797217da48538c
HwangHyeonseok <hhs0995@naver.com>
2025-03-28 14:36:31 +0900

    lecture: 8-1_AOP가 필요한 상황 이해하기 (직장 상사 : "우리 비즈니스 로직이 많이 느린거 같은데, 1000개의 메서드에 대해 각각의 실행 시간을 알아와라!")

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -21,11 +21,19 @@ public class MemberService {
 
     // 회원 가입
     public Long join(Member member) {
+        long start = System.currentTimeMillis();
+
         // 같은 이름이 있는 중복 회원 X
-        Optional<Member> result = memberRepository.findByName(member.getName());
-        validateDuplicateMember(result);
-        memberRepository.save(member);
-        return member.getId();
+        try {
+            Optional<Member> result = memberRepository.findByName(member.getName());
+            validateDuplicateMember(result);
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish-start;
+            System.out.println("join = " + timeMs + "ms");
+        }
     }
 
     // 중복 회원 검증
@@ -37,7 +45,14 @@ public class MemberService {
 
     // 전체 회원 조회
     public List<Member> findMembers() {
-        return memberRepository.findAll();
+        long start = System.currentTimeMillis();
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers = " + timeMs + "ms");
+        }
     }
 
     // ID에 맞는 1명 조회
